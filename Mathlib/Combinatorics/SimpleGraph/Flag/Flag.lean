4fc9b2a9a7521b0b379aa63d4cdec81097c6e82f
jt496 <john.talbot@gmail.com>
2025-10-01 10:01:00 +0100

    inducible

--- Mathlib/Combinatorics/SimpleGraph/Flag/Flag.lean
+++ Mathlib/Combinatorics/SimpleGraph/Flag/Flag.lean
@@ -27,7 +27,7 @@ set_option linter.style.header false
 -/
 
 local notation "‖" x "‖" => Fintype.card x
-
+open Finset
 
 
 namespace SimpleGraph
@@ -35,22 +35,22 @@ namespace SimpleGraph
 /-- A graph property -/
 abbrev GraphProp.{u} := {α : Type u} → SimpleGraph α → Prop
 
-abbrev GraphType := {α : Type*} → [Fintype α] → SimpleGraph α → Type*
+abbrev GraphType := {α  β : Type*} → [Fintype α] → SimpleGraph α → SimpleGraph β → Type*
 
 universe u
 
-def Finitary (T : GraphType) : Prop :=
-  ∀ {α : Type u}, ∀ [Fintype α], ∀ G : SimpleGraph α, Finite (T G)
+-- def Finitary (T : GraphType) : Prop :=
+--   ∀ {α : Type u}, ∀ [Fintype α], ∀ G : SimpleGraph α, Finite (T G)
 
-def Inducible (T : GraphType) : Prop := ∀ {α : Type u}, ∀ [Fintype α], ∀ G : SimpleGraph α,
-  ∀ t : Set α, ∀ [DecidablePred (· ∈ t)], Nonempty (T (G.induce t) ≃ T G)
-
+def Inducible (T : GraphType) : Prop := ∀ {α β : Type*}, ∀ [Fintype α], ∀ G : SimpleGraph α,
+  ∀ H : SimpleGraph β, ∀ [Fintype (T G H)], ∀ [∀ t : Finset α, Fintype (T (G.induce t) H)],
+    ∃ k l : ℕ, ∑ t : Finset α with #t = k, ‖T (G.induce t) H‖ = l * ‖T G H‖
 
 variable {α β : Type*} [Fintype α] (H : SimpleGraph β) (G : SimpleGraph α)
 
-lemma embedding_finitary {β : Type*} (H : SimpleGraph β) : Finitary (fun G => (H ↪g G)) := by
-  intro α _ G
-  sorry
+-- lemma embedding_finitary {β : Type*} (H : SimpleGraph β) : Finitary (fun G => (H ↪g G)) := by
+--   intro α _ G
+--   sorry
 
 namespace GraphProp
 
